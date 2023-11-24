6b99dbbb23b8c813245d4b6d992fed20d0885ec5
vladee <vvv@>
2023-11-24 18:33:12 +0000

    lesson 3

---
+++ matrix.py
@@ -0,0 +1,9 @@
+Input: [[1,2],[3,4],[5,6]]
+Output: [[1,3,5],[2,4,6]]
+Explanation: Suppose we are given a matrix
+                       [[1, 2],
+                        [3, 4],
+                        [5, 6]]
+Then the transpose of the given matrix will be,
+                       [[1, 3, 5],
+                        [2, 4, 6]]
