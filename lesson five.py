1c6cdd70f4aef312414a5f1679f495a756f67c78
vladee <vvv@>
2023-12-01 18:13:45 +0000

    lesson 3

---
+++ lesson five.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+
+import requests
+
+url_todos = 'https://dummyjson.com/todos/'
+
+response = requests.get(url=url_todos)
+# print(response)
+# print(response.content)
+# print(response.text)
+data_from_net = response.json()
+
+todos = data_from_net['todos']
+
+for todo in todos:
+    print(todo)
