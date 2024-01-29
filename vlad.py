34198bb6949954d9a12c2c6d71dfe6ada8b1f30a
vladee <vvv@>
2024-01-29 16:57:25 +0000

    lesson 3

--- vlad.py
+++ vlad.py
@@ -1,20 +1,12 @@
-# Оголошуємо функцію, яка приймає список цілих чисел як параметр
-def sum_even_and_multiply (lst):
-  # Якщо список порожній, повертаємо 0
-  if not lst:
+def calculate(nums):
+  if not nums:
     return 0
-  # Ініціалізуємо змінну для зберігання суми елементів з парними індексами
-  even_sum = 0
-  # Проходимо по всіх елементах списку, використовуючи індексацію
-  for i in range(len(lst)):
-    # Якщо індекс парний, додаємо елемент до суми
-    if i % 2 == 0:
-      even_sum += lst[i]
-  # Повертаємо добуток суми і останнього елемента списку
-  return even_sum * lst[-1]
+  else:
+    return sum(nums[i] for i in range(0, len(nums), 2)) * nums[-1]
 
-# Тестуємо функцію на прикладах
-print(sum_even_and_multiply([0, 1, 7, 2, 4, 8])) # 88
-print(sum_even_and_multiply([1, 3, 5])) # 30
-print(sum_even_and_multiply([6])) # 36
-print(sum_even_and_multiply([])) # 0
+
+
+print(calculate([0, 1, 7, 2, 4, 8]))
+print(calculate([1, 3, 5]))
+print(calculate([6]))
+print(calculate([]))
