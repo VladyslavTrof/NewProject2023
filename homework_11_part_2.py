a4fbe35539a3c2723d8c5553437101daebde9f3c
vladee <vvv@>
2024-02-21 20:18:09 +0000

    lesson 3

--- homework_11_part_2.py
+++ homework_11_part_2.py
@@ -5,3 +5,10 @@ assert is_even(2494563894038**2) == True, 'Test1'
 assert is_even(1056897**2) == False, 'Test2'
 assert is_even(24945638940387**3) == False, 'Test3'
 print('Ok')
+
+def is_even(number):
+    return bin(number)[-1] == '0'
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+print('Ok')
